336e8d474762c9edb66168baf2d7454a9aef16f4
Paul Yuryev <paul.yuryev@gmail.com>
2013-06-08 02:31:44 +0300

    Screen update #commit

--- BGJ05/src/org/bgj05/main/MainComponent.java
+++ BGJ05/src/org/bgj05/main/MainComponent.java
@@ -1,5 +1,6 @@
 package org.bgj05.main;
 
+import java.util.Random;
 import org.isjaki.*;
 import org.lwjgl.opengl.GL11;
 
@@ -7,20 +8,31 @@ public class MainComponent extends Base2DGame {
 	public static int SCREEN_WIDTH 	= 1280;
 	public static int SCREEN_HEIGHT	= 720;
 	
+	/* This is the part where I wish Java inherited
+	 * properties from C#, oh god why not omfg */
+	private static ScreenManager screenManager;
+	public static ScreenManager screenManager() { return screenManager; }
+	
+	private static Random randomGenerator;
+	public static Random randomGenerator() { return randomGenerator; } 
+	
 	public MainComponent() {
 		super("BGJ 05", SCREEN_WIDTH, SCREEN_HEIGHT, true, false);
 	}
 
 	protected void init() {
-		
+		screenManager = new ScreenManager();
+		randomGenerator = new Random();
 	}
 	
 	protected void update() {
 		Isjaki.update();
+		screenManager.currentScreen().update();
 	}
 	
 	protected void render() {
 		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+		screenManager.currentScreen().render();
 	}
 	
 	public static void main(String[] arguments) {
